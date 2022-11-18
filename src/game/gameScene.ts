7fdae5cdf95abe9638a9fa924a9b9f5d7200b297
arybary <l3fljl5ldydy@gmail.com>
2022-11-18 22:30:38 +0200

    positions code razdel

--- src/game/gameScene.ts
+++ src/game/gameScene.ts
@@ -1,6 +1,7 @@
 import { Spine } from "pixi-spine";
-import { Container, Sprite, TextStyle, Texture, Text, Loader } from "pixi.js";
+import { Container, Sprite, Texture, Loader } from "pixi.js";
 import { getEnemy, killEnemy } from "./enemy";
+import { getScore } from "./scoreBar";
 
 export function getGameScene(): Container {
     const enemiesPositions = Loader.shared.resources.positions.data;
@@ -10,7 +11,6 @@ export function getGameScene(): Container {
     const bg = new Sprite(Texture.from("background.png"));
     bg.anchor.set(0, 0);
     gameScene.addChild(bg);
-    gameScene.cursor = "url('../../assets/bunny.png'),auto";
 
     const scoreBar = new Container();
     scoreBar.position.set(gameScene.width / 2 - scoreBar.width / 2, 22);
@@ -19,38 +19,33 @@ export function getGameScene(): Container {
     const bgScoreBar = new Sprite(Texture.from("score.png"));
     scoreBar.addChild(bgScoreBar);
 
-    const style = new TextStyle({
-        fontFamily: "Arial",
-        fontSize: 28,
-        fill: "white",
-    });
+    const scoreEnemy = getScore(enemiesAmount);
+    scoreBar.addChild(scoreEnemy);
 
-    const score = new Text(enemiesAmount, style);
-    score.x = -score.width / 2;
-    score.y = -score.height / 2 - 1;
-    scoreBar.addChild(score);
-
-    function handlerClickKillEnemy(enemy: Spine) {
+    const handlerClickKillEnemy = (enemy: Spine) => {
         enemiesAmount -= 1;
-        score.text = enemiesAmount;
-        score.x = -score.width / 2;
-        score.y = -score.height / 2;
+        scoreEnemy.text = enemiesAmount;
+
         gameScene.removeChild(enemy);
+
         const xPos = enemy.x - 50;
         const yPos = enemy.y - 50;
         const kill = killEnemy(xPos, yPos);
         gameScene.addChild(kill);
+
         if (enemiesAmount === 0) {
             alert(`Game END`);
             location.reload();
         }
-    }
+    };
+
     enemiesPositions.forEach((pos: { x: number; y: number }) => {
         const { x, y } = pos;
 
         const enemy = getEnemy(x, y);
 
         gameScene.addChild(enemy);
+
         enemy.on("pointerdown", () => handlerClickKillEnemy(enemy));
     });
 
---
+++ src/game/scoreBar.ts
@@ -0,0 +1,15 @@
+import { TextStyle, Text } from "pixi.js";
+
+export const getScore = (amount: number) => {
+    const style = new TextStyle({
+        fontFamily: "Arial",
+        fontSize: 28,
+        fill: "white",
+    });
+
+    const score = new Text(amount, style);
+    score.x = -score.width / 2;
+    score.y = -score.height / 2 - 1;
+
+    return score;
+};
